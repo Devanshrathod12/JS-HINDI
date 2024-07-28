78cbba28147e13eb9bd072d87a0a3374d2df52e8
devansh rathod <rathoddevansh53@gmail.com>
2024-07-28 13:08:52 +0530

    convertionoprations

---
+++ 01_basics/03_convertionandoprations.js
@@ -0,0 +1,30 @@
+// let Score = false;
+
+// console.log(typeof Score);
+// console.log(typeof(Score));
+
+// let ValueInNumber = Number(Score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+ 
+
+
+// 33 => 33;
+// "33as" => NaN 
+// ture=1; false=0;
+
+
+let islogeddin = "dddd";
+
+let Booleanislogeddin = Boolean(islogeddin)
+//console.log(Booleanislogeddin);
+
+
+// 1 => true; 0 => false;
+// "" => false;
+// "dddd" => true;
+
+
+let Somenumber = 33;
+let stringNumber = String(Somenumber)
+console.log(typeof stringNumber);
