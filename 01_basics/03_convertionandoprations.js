a435768e1132042a73be24bcbc2730352e218782
devansh rathod <rathoddevansh53@gmail.com>
2024-07-28 13:45:37 +0530

    comperision

--- 01_basics/03_convertionandoprations.js
+++ 01_basics/03_convertionandoprations.js
@@ -28,3 +28,29 @@ let Booleanislogeddin = Boolean(islogeddin)
 let Somenumber = 33;
 let stringNumber = String(Somenumber)
 console.log(typeof stringNumber);
+
+
+
+// ************************** oprationn *****************************************
+
+
+// let value = 3;
+// let NegValue = -value;
+// console.log(NegValue);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let a = 2;
+let b =5;
+let c = a + b;
+console.log(c);
+
+console.log("2" + 2);
+console.log(2 + "2");
+console.log(2 + 2 + "2");
