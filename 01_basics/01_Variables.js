d1d907c6ab9cc6d45ca3d4070a7359d4397c7d29
devansh rathod <rathoddevansh53@gmail.com>
2024-07-28 12:26:20 +0530

    DataTypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 1344556;
+let accountEmail = "Devansh@google.com";
+var accountPassword = "88888";
+accountCity = "indore"
+let accountState;
+
+// accountId = 2; // not aalowed to changes
+
+/*
+prefer to not use var becouse
+isshu if black scope and funtional scope
+*/
+
+ accountEmail = "Dh@google.com";
+ accountPassword = "88555555888";
+accountCity = "bengluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // trite a all js code us a newer versoin
+
+ // alert(3 + 3); use node.js not browser 
+
+ let name = "devansh";
+ let age = 20;
+ let isMarried = false;
+
+ // datatype of js 
+ // number => 2 power 53
+ //string = "devansh"
+ //boolean = false,true
+ // null = standalone value
+ // undefined = variable is declared but not assigned a value
+ // symbol => unique
+
+ // object = collection of key value pairs
+
+ console.log(typeof null); // null type is object 
+ console.log(typeof undefined);// undifind type is  undifaind
